29709919302c22592eb23822ce0ed2fac075ed0b
Aisana Nurgali <nurgaliaisana1@gmail.com>
2026-04-08 00:43:19 +0500

    feat: initial lab10 structure with task1

---
+++ task1/jest.config.js
@@ -0,0 +1,17 @@
+export default {
+    preset: "ts-jest",
+    testEnvironment: "jsdom",
+    setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+    moduleNameMapper: {
+      "\\.(css|less|scss|sass)$": "identity-obj-proxy"
+    },
+    transform: {
+      "^.+\\.(ts|tsx)$": [
+        "ts-jest",
+        {
+          tsconfig: "tsconfig.test.json",
+          useESM: true
+        }
+      ]
+    }
+  };
